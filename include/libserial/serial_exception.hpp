24dc6f4a4e6b3bca9eeb49fe849d9b7995266e07
Nestor Neto <nestor-dp@hotmail.com>
2025-10-16 14:13:27 -0300

    📝 docs(serial_exceptions): Update doxygen comments

--- include/libserial/serial_exception.hpp
+++ include/libserial/serial_exception.hpp
@@ -8,6 +8,16 @@
 #include <utility>
 
 namespace libserial {
+/**
+ * @class SerialException
+ * @brief Exception class for serial port errors
+ *
+ * The SerialException class is used to represent errors that occur
+ * during serial port operations. It inherits from std::exception and
+ * provides a descriptive error message.
+ *
+ * @author Nestor Pereira Neto
+ */
 class SerialException : public std::exception {
 public:
 explicit SerialException(std::string message) : message_(std::move(message)) {
