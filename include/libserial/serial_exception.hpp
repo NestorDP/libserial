ebc59a143316dd4ce497f48e0a2f7b449c02d302
Nestor Neto <nestor-dp@hotmail.com>
2025-10-20 21:08:00 -0300

    🐛 fix(exception): Remove overite of class base message

--- include/libserial/serial_exception.hpp
+++ include/libserial/serial_exception.hpp
@@ -28,60 +28,54 @@ const char* what() const noexcept override {
 }
 private:
 std::string message_;
-}; // class SerialException
+};  // class SerialException
 
 /**
  * @class PortNotFoundException
  * @brief Exception class for port not found errors
- * 
+ *
  * The PortNotFoundException class is derived from SerialException
  * and is used to indicate that a specified serial port could not be found.
  */
 class PortNotFoundException : public SerialException {
 public:
 explicit PortNotFoundException(std::string message)
-        : SerialException(std::move(message)) {}
-    const char* what() const noexcept override { return message_.c_str(); }
-private:
-std::string message_;
+  : SerialException(std::move(message)) {
+}
 };  // class PortNotFoundException
 
 /**
  * @class PermissionDeniedException
  * @brief Exception class for permission denied errors
- * 
+ *
  * The PermissionDeniedException class is derived from SerialException
  * and is used to indicate that permission to access a specified serial port was denied.
  */
 class PermissionDeniedException : public SerialException {
 public:
 explicit PermissionDeniedException(std::string message)
-        : SerialException(std::move(message)) {}
-    const char* what() const noexcept override { return message_.c_str(); }
-private:
-std::string message_;
+  : SerialException(std::move(message)) {
+}
 };  // class PermissionDeniedException
 
 /**
  * @class TimeoutException
  * @brief Exception class for timeout errors
- * 
+ *
  * The TimeoutException class is derived from SerialException
  * and is used to indicate that a serial port operation has timed out.
  */
 class TimeoutException : public SerialException {
 public:
 explicit TimeoutException(std::string message)
-        : SerialException(std::move(message)) {}
-    const char* what() const noexcept override { return message_.c_str(); }
-private:
-std::string message_;
+  : SerialException(std::move(message)) {
+}
 };  // class TimeoutException
 
 /**
  * @class IOException
  * @brief Exception class for I/O errors
- * 
+ *
  * The IOException class is derived from SerialException
  * and is used to indicate that an I/O error has occurred during
  * serial port operations.
@@ -89,12 +83,10 @@ std::string message_;
 class IOException : public SerialException {
 public:
 explicit IOException(std::string message)
-        : SerialException(std::move(message)) {}
-    const char* what() const noexcept override { return message_.c_str(); }
-private:
-std::string message_;
+  : SerialException(std::move(message)) {
+}
 };  // class IOException
 
-};  // namespace libserial
+}  // namespace libserial
 
 #endif  // INCLUDE_LIBSERIAL_SERIAL_EXCEPTION_HPP_
